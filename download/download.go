721ce661cdfd3cb96aba75250a8f488f67d467dc
Lucian Maly <lucky@senior.cz>
2019-01-12 14:12:56 +1100

    Getting authenticity_token

--- download/download.go
+++ download/download.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -11,26 +10,55 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// variables
+var email = "XXXX"    // !!! change to your e-mail address
+var password = "XXXX" // !!! enter your password here
+
 func main() {
+	// open the file
 	file, err := os.Open("books_urls.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		c := colly.NewCollector()
-		c.OnHTML("body table tbody tr td", func(e *colly.HTMLElement) {
-			dataBookID := e.Attr("data-book-id")
+	// authenticate
+	fmt.Println("Logging in...")
+	// get the authenticity token
+	c := colly.NewCollector()
+	c.OnHTML("div.loginV2--login div[class=loginV2__form] input[type=hidden][name=authenticity_token]", func(e *colly.HTMLElement) {
+		authenticityToken := e.Attr("value")
+		fmt.Println("authenticity_token is", authenticityToken)
+		err := c.Post("https://www.blinkist.com/en/nc/login", map[string]string{"login[email]": email, "login[password]": password, "authenticity_token": authenticityToken})
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	// attach callbacks after login
+	c.OnResponse(func(r *colly.Response) {
+		log.Print("response received HTTP", r.StatusCode)
+	})
+	c.Visit("https://www.blinkist.com/en/nc/login")
 
-			// Print
-			fmt.Println(dataBookID)
-		})
-		c.Visit(scanner.Text())
-	}
+	// read line in the file
+	//scanner := bufio.NewScanner(file)
+	//for scanner.Scan() {
+	// create a new collector
+	//c := colly.NewCollector()
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// scrape it baby scrape it
+	// c.OnHTML("body", func(e *colly.HTMLElement) {
+	//e.ForEach("span", func(_ int, elem *colly.HTMLElement) {
+	//if strings.Contains(elem.Text, "") {
+	// 	dataBookID := e.Text
+	// 	fmt.Println(dataBookID)
+	// })
+
+	// visit
+	// c.Visit(scanner.Text())
+	//}
+
+	//if err := scanner.Err(); err != nil {
+	//	log.Fatal(err)
+	//}
 }
