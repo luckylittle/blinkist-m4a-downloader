a269db902546467794d2858a6f97b5bace88ebbe
Lucian Maly <lucky@senior.cz>
2019-01-12 22:49:07 +1100

    Scraping title + title id and chapters + chapter ids

--- download/download.go
+++ download/download.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -22,43 +23,79 @@ func main() {
 	}
 	defer file.Close()
 
-	// authenticate
-	fmt.Println("Logging in...")
-	// get the authenticity token
-	c := colly.NewCollector()
-	c.OnHTML("div.loginV2--login div[class=loginV2__form] input[type=hidden][name=authenticity_token]", func(e *colly.HTMLElement) {
-		authenticityToken := e.Attr("value")
-		fmt.Println("authenticity_token is", authenticityToken)
-		err := c.Post("https://www.blinkist.com/en/nc/login", map[string]string{"login[email]": email, "login[password]": password, "authenticity_token": authenticityToken})
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-	// attach callbacks after login
-	c.OnResponse(func(r *colly.Response) {
-		log.Print("response received HTTP", r.StatusCode)
-	})
-	c.Visit("https://www.blinkist.com/en/nc/login")
-
 	// read line in the file
-	//scanner := bufio.NewScanner(file)
-	//for scanner.Scan() {
-	// create a new collector
-	//c := colly.NewCollector()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
 
-	// scrape it baby scrape it
-	// c.OnHTML("body", func(e *colly.HTMLElement) {
-	//e.ForEach("span", func(_ int, elem *colly.HTMLElement) {
-	//if strings.Contains(elem.Text, "") {
-	// 	dataBookID := e.Text
-	// 	fmt.Println(dataBookID)
-	// })
+		// instantiate default collector
+		c := colly.NewCollector()
 
-	// visit
-	// c.Visit(scanner.Text())
-	//}
+		// get the authenticity token
+		c.OnHTML("div.loginV2--login div[class=loginV2__form] input[type=hidden][name=authenticity_token]", func(e *colly.HTMLElement) {
+			authenticityToken := e.Attr("value")
+			fmt.Println("Logging in...")
+			fmt.Println("authenticity_token for", scanner.Text(), "is", authenticityToken)
 
-	//if err := scanner.Err(); err != nil {
-	//	log.Fatal(err)
-	//}
+			// authenticate
+			err := c.Post("https://www.blinkist.com/en/nc/login/", map[string]string{"utf8": "&#x2713;", "authenticity_token": authenticityToken, "login[google_id_token]": "", "login[facebook_access_token]": "", "login[email]": email, "login[password]": password})
+			if err != nil {
+				log.Fatal(err)
+			}
+		})
+
+		// visit login page
+		c.Visit("https://www.blinkist.com/en/nc/login/")
+
+		// attach callbacks after login
+		c.OnResponse(func(r *colly.Response) {
+			log.Print("Login response received HTTP", r.StatusCode)
+			log.Println("Visited", r.Request.URL)
+		})
+
+		// attach error after login
+		c.OnError(func(_ *colly.Response, err error) {
+			log.Println("Something went wrong:", err)
+		})
+
+		// create another collector to scrape book details
+		bookCollector := c.Clone()
+
+		// scrape it baby scrape it!
+		// read book title
+		bookCollector.OnHTML("div[class=reader__container__share] a[class=share__facebook-icon]", func(f *colly.HTMLElement) {
+			dataTitle := f.Attr("data-title")
+			fmt.Println("Book title is:", dataTitle)
+		})
+
+		// read book ID
+		bookCollector.OnHTML("div[class=reader__container]", func(g *colly.HTMLElement) {
+			dataBookID := g.Attr("data-book-id")
+			fmt.Println("Book ID is:", dataBookID)
+		})
+
+		// read chapters and corresponding IDs
+		bookCollector.OnHTML("div.chapter", func(h *colly.HTMLElement) {
+			dataChapterNo := h.Attr("data-chapterno")
+			dataChapterID := h.Attr("data-chapterid")
+			fmt.Println("Chapter", dataChapterNo, "has ID:", dataChapterID)
+		})
+
+		// attach callbacks after data title
+		bookCollector.OnResponse(func(f *colly.Response) {
+			log.Print("Book page response received HTTP", f.StatusCode)
+		})
+
+		// visit
+		fmt.Println("Visiting", scanner.Text())
+		bookCollector.Visit(scanner.Text())
+
+		bookCollector.OnScraped(func(r *colly.Response) {
+			fmt.Println("Finished", r.Request.URL)
+		})
+	}
+
+	// if reading the file fails
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
